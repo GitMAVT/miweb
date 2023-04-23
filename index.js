c5579c1efca43d23143c55f1db05935be1c98091
Mauricio Vargas <mauricio.vargas.t@gmail.com>
2023-04-22 21:29:40 -0400

    Se agregaron archivos para aprender

---
+++ index.js
@@ -0,0 +1,16 @@
+let Gor = `I'm Mauricio`;
+
+
+
+/*
+    Tipos de datos
+    String: Cadena de caracteres.
+    Number: número entero o decimal.
+    Bolean: verdadero o falso.
+    Null: nulo.
+    Undefined: No definida pero si declarado.
+    Object: Objeto.
+    NaN: Not a Number (Aparece cuando se intenta realizar una operación con algo que no es un número)
+*/
+let miPrimeraVariable = "Mi primera variable!"
+console.log(miPrimeraVariable);
